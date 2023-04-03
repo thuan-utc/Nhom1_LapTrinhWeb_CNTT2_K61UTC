819720f762705ea4e8fb54187ecead4056edf11b
thuan-utc <thuansl99@gmail.com>
2023-04-03 22:34:49 +0700

    initial admin and cooperator areas

---
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nhom1_LapTrinhWeb_CNTT2_K61.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
