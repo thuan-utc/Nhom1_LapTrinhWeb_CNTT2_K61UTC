0a47ecba3fe417878482aac227b7b3cd22711e0c
Bá Tuấn <batuan1112002@gmail.com>
2023-04-06 20:17:21 +0700

    Tuan update Edit

--- Areas/Admin/Controllers/HomeAdminController.cs
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Nhom1_LapTrinhWeb_CNTT2_K61.Models;
 
 namespace Nhom1_LapTrinhWeb_CNTT2_K61.Areas.Admin.Controllers
@@ -39,6 +40,29 @@ namespace Nhom1_LapTrinhWeb_CNTT2_K61.Areas.Admin.Controllers
             if (ModelState.IsValid)
             {
                 db.Tours.Add(tour);
+                db.SaveChanges();
+                return RedirectToAction("ListTour");
+            }
+            return View(tour);
+        }
+
+        [Route("editTour")]
+        [HttpGet]
+        public IActionResult EditTour(string maTour)
+        {
+            ViewBag.MaNv = new SelectList(db.NhanViens.ToList(), "MaNv", "MaNv");
+            var tour = db.Tours.Find(maTour);
+            return View(tour);
+        }
+
+        [Route("editTour")]
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult EditTour(Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tour).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("ListTour");
             }
