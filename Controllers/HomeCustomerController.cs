9c92a72ffa9b89932f7be5cd2fdd3545737cc8a2
thuan-utc <thuansl99@gmail.com>
2023-04-18 16:12:11 +0700

    add accont profile

--- Controllers/HomeCustomerController.cs
+++ Controllers/HomeCustomerController.cs
@@ -10,117 +10,120 @@ using X.PagedList;
 namespace Nhom1_LapTrinhWeb_CNTT2_K61.Controllers
 {
     [Route("customer")]
-	public class HomeCustomerController : Controller
-	{
-		TourManagementContext tourDb = new TourManagementContext();
-		private readonly ILogger<HomeController> _logger;
-		[Route("index")]
-		[Route("")]
-		
-		public IActionResult Index(int? page)
-		{
-			int pageNumber = page == null || page < 1 ? 1 : page.Value;
-			int pageSize = 9;
-			var listTour = tourDb.Tours;
+    public class HomeCustomerController : Controller
+    {
+        TourManagementContext tourDb = new TourManagementContext();
+        private readonly ILogger<HomeController> _logger;
+
+        [Route("index")]
+        [Route("")]
+        public IActionResult Index(int? page)
+        {
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSize = 9;
+            var listTour = tourDb.Tours;
             var kh = getCurrentUser();
             ViewBag.tenKhach = kh.TenKh;
             var listSanPham = tourDb.Tours.AsNoTracking().OrderBy(x => x.TenTour);
-			PagedList<Tour> lst = new PagedList<Tour>(listSanPham, pageNumber, pageSize);
-			return View(lst);
-		}
-		[Route("packages")]
-		public IActionResult Packages(int? page)
-		{
-			int pageNumber = page == null || page < 1 ? 1 : page.Value;
-			int pageSize = 9;
-			var listTour = tourDb.Tours;
-			var listSanPham = tourDb.Tours.AsNoTracking().OrderBy(x => x.TenTour);
-			PagedList<Tour> lst = new PagedList<Tour>(listSanPham, pageNumber, pageSize);
-			return View(lst);
-		}
-		[Route("tourThaiLan")]
-		public IActionResult TourThaiLan(int? page)
-		{
-			int pageNumber = page == null || page < 1 ? 1 : page.Value;
-			int pageSize = 9;
-			var listTour = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == 6).ToList();
-			PagedList<Tour> lst = new PagedList<Tour>(listTour, pageNumber, pageSize);
-			ViewBag.MaQg = 6;
-			return View(lst);
-		}
-		[Route("tourSingapore")]
-		public IActionResult TourSingapore(int? page)
-		{
-			int pageNumber = page == null || page < 1 ? 1 : page.Value;
-			int pageSize = 9;
-			var listTour = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == 9).ToList();
-			PagedList<Tour> lst = new PagedList<Tour>(listTour, pageNumber, pageSize);
-			ViewBag.MaQg = 9;
-			return View(lst);
-		}
-		[Route("tourChina")]
-		public IActionResult TourChina(int? page)
-		{
-			int pageNumber = page == null || page < 1 ? 1 : page.Value;
-			int pageSize = 9;
-			var listTour = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == 1).ToList();
-			PagedList<Tour> lst = new PagedList<Tour>(listTour, pageNumber, pageSize);
-			ViewBag.MaQg = 1;
-			return View(lst);
-		}
-		[Route("tourAnh")]
-		public IActionResult TourAnh(int? page)
-		{
-			int pageNumber = page == null || page < 1 ? 1 : page.Value;
-			int pageSize = 9;
-			var listTour = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == 5).ToList();
-			PagedList<Tour> lst = new PagedList<Tour>(listTour, pageNumber, pageSize);
-			ViewBag.MaQg = 5;
-			return View(lst);
-		}
+            PagedList<Tour> lst = new PagedList<Tour>(listSanPham, pageNumber, pageSize);
+            return View(lst);
+        }
+        [Route("packages")]
+        public IActionResult Packages(int? page)
+        {
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSize = 9;
+            var listTour = tourDb.Tours;
+            var listSanPham = tourDb.Tours.AsNoTracking().OrderBy(x => x.TenTour);
+            PagedList<Tour> lst = new PagedList<Tour>(listSanPham, pageNumber, pageSize);
+            return View(lst);
+        }
+
+        [Route("tourThaiLan")]
+        public IActionResult TourThaiLan(int? page)
+        {
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSize = 9;
+            var listTour = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == 6).ToList();
+            PagedList<Tour> lst = new PagedList<Tour>(listTour, pageNumber, pageSize);
+            ViewBag.MaQg = 6;
+            return View(lst);
+        }
+
+        [Route("tourSingapore")]
+        public IActionResult TourSingapore(int? page)
+        {
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSize = 9;
+            var listTour = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == 9).ToList();
+            PagedList<Tour> lst = new PagedList<Tour>(listTour, pageNumber, pageSize);
+            ViewBag.MaQg = 9;
+            return View(lst);
+        }
+
+        [Route("tourChina")]
+        public IActionResult TourChina(int? page)
+        {
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSize = 9;
+            var listTour = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == 1).ToList();
+            PagedList<Tour> lst = new PagedList<Tour>(listTour, pageNumber, pageSize);
+            ViewBag.MaQg = 1;
+            return View(lst);
+        }
+
+        [Route("tourAnh")]
+        public IActionResult TourAnh(int? page)
+        {
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSize = 9;
+            var listTour = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == 5).ToList();
+            PagedList<Tour> lst = new PagedList<Tour>(listTour, pageNumber, pageSize);
+            ViewBag.MaQg = 5;
+            return View(lst);
+        }
 
 
-		[Route("tourDetail")]
+        [Route("tourDetail")]
+        public IActionResult TourDetail(int matour)
+        {
+            var tourDetail = (from t in tourDb.Tours
+                              join ct in tourDb.Cttours on t.MaTour equals ct.MaTour
+                              where t.MaTour.Equals(matour)
+                              select new { Tour = t, Cttour = ct }).ToList();
+            ViewBag.TourDetail = tourDetail;
+            return View();
+        }
+        [Route("booking")]
+        [HttpGet]
+        public IActionResult Booking(int tourId)
+        {
+            var tourDetail = (from t in tourDb.Tours
+                              join ct in tourDb.Cttours on t.MaTour equals ct.MaTour
+                              where t.MaTour == tourId
+                              select new
+                              {
+                                  t.MaTour,
+                                  t.TenTour,
+                                  t.Gia,
+                                  ct.ThoiGian,
+                                  ct.KhachSan,
+                                  ct.AmThuc,
+                                  ct.PhuongTien,
+                                  ct.DoiTuongTh,
+                                  ct.UuDai,
+                                  t.AnhTour,
+                                  t.NgayBd,
+                                  t.NgayKt,
+                                  t.Sltcl
+                              }).ToList();
+            ViewBag.tourDetail = tourDetail;
+            ViewBag.khachHang = getCurrentUser();
+            return View();
+        }
 
-		public IActionResult TourDetail(int matour)
-		{
-			var tourDetail = (from t in tourDb.Tours
-							   join ct in tourDb.Cttours on t.MaTour equals ct.MaTour
-							   where t.MaTour.Equals(matour)
-							   select new { Tour = t, Cttour = ct }).ToList();
-			ViewBag.TourDetail = tourDetail;
-			return View();
-		}
-		[Route("booking")]
-		[HttpGet]
-		public IActionResult Booking(int tourId)
-		{
-			var tourDetail = (from t in tourDb.Tours
-							   join ct in tourDb.Cttours on t.MaTour equals ct.MaTour
-							   where t.MaTour == tourId
-							   select new
-							   {
-								   t.MaTour,
-								   t.TenTour,
-								   t.Gia,
-								   ct.ThoiGian,
-								   ct.KhachSan,
-								   ct.AmThuc,
-								   ct.PhuongTien,
-								   ct.DoiTuongTh,
-								   ct.UuDai,
-								   t.AnhTour,
-								   t.NgayBd,
-								   t.NgayKt,
-								   t.Sltcl
-							   }).ToList();	
-			ViewBag.tourDetail = tourDetail;
-			ViewBag.khachHang = getCurrentUser();
-			return View();
-		}
-
-		[Route("booking")]
-		[HttpPost]
+        [Route("booking")]
+        [HttpPost]
         public IActionResult Booking(int soVe, int tourId)
         {
             if (soVe <= 0)
@@ -133,144 +136,138 @@ namespace Nhom1_LapTrinhWeb_CNTT2_K61.Controllers
                 return BadRequest();
             }
             var kh = getCurrentUser();
-			var hd = new HoaDon
-			{
-				MaKh = kh.MaKh,
-				MaTour = tour.MaTour,
-				MaDaiLy = tour.MaDaiLy,
-				TongTien = tour.Gia * soVe
+            var hd = new HoaDon
+            {
+                MaKh = kh.MaKh,
+                MaTour = tour.MaTour,
+                MaDaiLy = tour.MaDaiLy,
+                TongTien = tour.Gia * soVe
             };
             tourDb.HoaDons.Add(hd);
             tourDb.SaveChanges();
-			var cthd = new Cthd
-			{
-				MaHd = hd.MaHd,
-				Gia = tour.Gia,
-				SoVe = soVe
-			};
-			tour.Sltcl -= soVe;
-			tourDb.Cthds.Add(cthd);
-			tourDb.SaveChanges();
-			ViewBag.tenKhach = kh.TenKh;
-			ViewBag.sdt = kh.Sdt;
-			ViewBag.tenTour = tour.TenTour;
-			ViewBag.ngayBd = tour.NgayBd;
-			ViewBag.soVe = soVe;
-			ViewBag.tongTien = hd.TongTien;
-			ViewBag.ngayDat = hd.CreatedDate;
+            var cthd = new Cthd
+            {
+                MaHd = hd.MaHd,
+                Gia = tour.Gia,
+                SoVe = soVe
+            };
+            tour.Sltcl -= soVe;
+            tourDb.Cthds.Add(cthd);
+            tourDb.SaveChanges();
+            ViewBag.tenKhach = kh.TenKh;
+            ViewBag.sdt = kh.Sdt;
+            ViewBag.tenTour = tour.TenTour;
+            ViewBag.ngayBd = tour.NgayBd;
+            ViewBag.soVe = soVe;
+            ViewBag.tongTien = hd.TongTien;
+            ViewBag.ngayDat = hd.CreatedDate;
             return View("BookSuccessFully");
         }
+
         [Route("showAccount")]
-     
         public IActionResult ShowAccount()
-		{
-			var kh = getCurrentUser();
-			ViewBag.tenKhach = kh.TenKh;
-			return View();
+        {
+            var kh = getCurrentUser();
+            ViewBag.tenKhach = kh.TenKh;
+            return View();
 
         }
-		[Route("history")]
-		public IActionResult History(int MaKH = 0)
-		{
-			var kh = getCurrentUser();
-			ViewBag.tenKhach = kh.TenKh;
-			if (MaKH == 0)
-			{
-				MaKH = getMaKH();
-			}
-			var history = (from hd in tourDb.HoaDons
-						   join ts in tourDb.Tours on hd.MaTour equals ts.MaTour
-						   join cthd in tourDb.Cthds on hd.MaHd equals cthd.MaHd
-						   join Kh in tourDb.KhachHangs on hd.MaKh equals Kh.MaKh
-						   where String.Equals(Kh.MaKh, MaKH)
-						   select new { Tour = ts, HoaDon = hd, Cthd = cthd, KhachHang = Kh }).ToList();
-			ViewBag.History= history;
 
+        [Route("history")]
+        public IActionResult History(int MaKH = 0)
+        {
+            var kh = getCurrentUser();
+            ViewBag.tenKhach = kh.TenKh;
+            if (MaKH == 0)
+            {
+                MaKH = getMaKH();
+            }
+            var history = (from hd in tourDb.HoaDons
+                           join ts in tourDb.Tours on hd.MaTour equals ts.MaTour
+                           join cthd in tourDb.Cthds on hd.MaHd equals cthd.MaHd
+                           join Kh in tourDb.KhachHangs on hd.MaKh equals Kh.MaKh
+                           where String.Equals(Kh.MaKh, MaKH)
+                           select new { Tour = ts, HoaDon = hd, Cthd = cthd, KhachHang = Kh }).ToList();
+            ViewBag.History = history;
+            return View();
+        }
 
-			return View();
-		}
-		[Route("myAccount")]
-		public IActionResult MyAccount()
-		{
-			var kh = getCurrentUser();
-			ViewBag.tenKhach = kh.TenKh;
-			return View();
-		}
+        [Route("myAccount")]
+        public IActionResult MyAccount()
+        {
+            var kh = getCurrentUser();
+            ViewBag.tenKhach = kh.TenKh;
+            return View();
+        }
 
-		private int getMaKH()
-		{
-			int MaKHang = 0;
-			var userName = HttpContext.Session.GetString("UserName");
-			var khachhang = (from kh in tourDb.KhachHangs
-							 join tk in tourDb.TaiKhoans on kh.MaKh equals tk.MaKh
-							 where String.Equals(tk.Taikhoan1, userName)
-							 select kh).ToList();
-			if (khachhang.Count > 0)
-			{
-				MaKHang = khachhang[0].MaKh;
-			}
-			return MaKHang;
-		}
+        private int getMaKH()
+        {
+            int MaKHang = 0;
+            var userName = HttpContext.Session.GetString("UserName");
+            var khachhang = (from kh in tourDb.KhachHangs
+                             join tk in tourDb.TaiKhoans on kh.MaKh equals tk.MaKh
+                             where String.Equals(tk.Taikhoan1, userName)
+                             select kh).ToList();
+            if (khachhang.Count > 0)
+            {
+                MaKHang = khachhang[0].MaKh;
+            }
+            return MaKHang;
+        }
 
-
-
-
-
-
-		private KhachHang getCurrentUser()
+        private KhachHang getCurrentUser()
         {
             var userName = HttpContext.Session.GetString("UserName");
             var khachhang = (from kh in tourDb.KhachHangs
                              join tk in tourDb.TaiKhoans on kh.MaKh equals tk.MaKh
                              where String.Equals(tk.Taikhoan1, userName)
                              select kh).ToList();
-			return khachhang[0];
+            return khachhang[0];
         }
 
         [Route("tourTheoQuocGia")]
-		public IActionResult TourTheoQuocGia(int Maqg, int? page)
-		{
-			int pageNumber = page == null || page < 1 ? 1 : page.Value;
-			int pageSize = 9;
-			var listSanPham = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == Maqg).ToList();
-			PagedList<Tour> lst = new PagedList<Tour>(listSanPham, pageNumber, pageSize);
-			ViewBag.MaQg = Maqg;
-			return View(lst);
-		}
+        public IActionResult TourTheoQuocGia(int Maqg, int? page)
+        {
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            int pageSize = 9;
+            var listSanPham = tourDb.Tours.AsNoTracking().Where(x => x.MaQg == Maqg).ToList();
+            PagedList<Tour> lst = new PagedList<Tour>(listSanPham, pageNumber, pageSize);
+            ViewBag.MaQg = Maqg;
+            return View(lst);
+        }
 
+        [Route("search")]
+        public IActionResult Search(TourSearchModel model, int? page)
+        {
+            var tours = tourDb.Tours
+                .Where(t => t.NoiKhoiHanh == model.From
+                            && t.DiaDiem == model.To
+                            && t.NgayBd >= model.Checkin
+                            && t.NgayKt <= model.Checkout)
+                .ToList();
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            var pagedTours = tours.ToPagedList(pageNumber, pageSize);
 
-		[Route("search")]
-		public IActionResult Search(TourSearchModel model, int? page)
-		{
-			var tours = tourDb.Tours
-				.Where(t => t.NoiKhoiHanh == model.From
-							&& t.DiaDiem == model.To
-							&& t.NgayBd >= model.Checkin
-							&& t.NgayKt <= model.Checkout)
-				.ToList();
-			int pageSize = 10;
-			int pageNumber = (page ?? 1);
-			var pagedTours = tours.ToPagedList(pageNumber, pageSize);
+            return View(pagedTours);
+        }
 
-			return View(pagedTours);
-		}
+        [Route("aboutUs")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
 
-		[Route("aboutUs")]
-		public IActionResult AboutUs()
-		{
-			return View();
-		}
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
-		[Route("contact")]
-		public IActionResult Contact()
-		{
-			return View();
-		}
-
-		[Route("services")]
-		public IActionResult Services()
-		{
-			return View();
-		}
-	}
+        [Route("services")]
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
 }
